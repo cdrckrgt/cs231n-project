ba9bb1d3d8d763e15fe914ca97eb1934b15048c9
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-05-14 20:09:14 +0000

    stole pix2pix code from somebody and gonna adapt it to our stuff as baseline.

--- src/baseline.py
+++ src/baseline.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 
+from pix2pix import define_G, define_D, GANLoss, get_scheduler, update_learning_rate
+
 # Ignore warnings
 import warnings
 warnings.filterwarnings("ignore")
@@ -33,24 +35,106 @@ class SketchyDataset(Dataset):
 
         return sample
 
-sketch_dataset = SketchyDataset(root_dir='../data/sketchy_data/sketch/tx_000000000000/tree')
-photo_dataset = SketchyDataset(root_dir='../data/sketchy_data/photo/tx_000000000000/tree')
+transform = transforms.Compose([
+    transforms.RandomRotation(180)
+])
 
+sketch_dataset = SketchyDataset(root_dir='../data/tree_sketches', transform=transform)
+photo_dataset = SketchyDataset(root_dir='../data/tree_photos', transform=transform)
 
-fig = plt.figure()
+# taking code from https://github.com/mrzhu-cool/pix2pix-pytorch/blob/master/train.py
 
-for i in range(len(photo_dataset)):
-    sample = photo_dataset[i]
+device = torch.device('cuda' if torch.cuda_is_available() else 'cpu')
+print('running on ', device)
 
-    
-    plt.imshow(sample)
-    plt.pause(1)
+net_g = define_G(opt.input_nc, opt.output_nc, opt.ngf, 'batch', False, 'normal', 0.02, gpu_id=device)
+net_d = define_D(opt.input_nc + opt.output_nc, opt.ndf, 'basic', gpu_id=device)
 
-plt.close()
+criterionGAN = GANLoss().to(device)
+criterionL1 = nn.L1Loss().to(device)
+criterionMSE = nn.MSELoss().to(device)
 
-# augmentations
+# setup optimizer
+optimizer_g = optim.Adam(net_g.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+optimizer_d = optim.Adam(net_d.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+net_g_scheduler = get_scheduler(optimizer_g, opt)
+net_d_scheduler = get_scheduler(optimizer_d, opt)
 
-# pix2pix model
-#   as baseline
+for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
+    # train
+    for iteration, batch in enumerate(training_data_loader, 1):
+        # forward
+        real_a, real_b = batch[0].to(device), batch[1].to(device)
+        fake_b = net_g(real_a)
 
+        ######################
+        # (1) Update D network
+        ######################
 
+        optimizer_d.zero_grad()
+        
+        # train with fake
+        fake_ab = torch.cat((real_a, fake_b), 1)
+        pred_fake = net_d.forward(fake_ab.detach())
+        loss_d_fake = criterionGAN(pred_fake, False)
+
+        # train with real
+        real_ab = torch.cat((real_a, real_b), 1)
+        pred_real = net_d.forward(real_ab)
+        loss_d_real = criterionGAN(pred_real, True)
+        
+        # Combined D loss
+        loss_d = (loss_d_fake + loss_d_real) * 0.5
+
+        loss_d.backward()
+       
+        optimizer_d.step()
+
+        ######################
+        # (2) Update G network
+        ######################
+
+        optimizer_g.zero_grad()
+
+        # First, G(A) should fake the discriminator
+        fake_ab = torch.cat((real_a, fake_b), 1)
+        pred_fake = net_d.forward(fake_ab)
+        loss_g_gan = criterionGAN(pred_fake, True)
+
+        # Second, G(A) = B
+        loss_g_l1 = criterionL1(fake_b, real_b) * opt.lamb
+        
+        loss_g = loss_g_gan + loss_g_l1
+        
+        loss_g.backward()
+
+        optimizer_g.step()
+
+        print("===> Epoch[{}]({}/{}): Loss_D: {:.4f} Loss_G: {:.4f}".format(
+            epoch, iteration, len(training_data_loader), loss_d.item(), loss_g.item()))
+
+    update_learning_rate(net_g_scheduler, optimizer_g)
+    update_learning_rate(net_d_scheduler, optimizer_d)
+
+    # test
+    avg_psnr = 0
+    for batch in testing_data_loader:
+        input, target = batch[0].to(device), batch[1].to(device)
+
+        prediction = net_g(input)
+        mse = criterionMSE(prediction, target)
+        psnr = 10 * log10(1 / mse.item())
+        avg_psnr += psnr
+    print("===> Avg. PSNR: {:.4f} dB".format(avg_psnr / len(testing_data_loader)))
+
+    #checkpoint
+    if epoch % 50 == 0:
+        if not os.path.exists("checkpoint"):
+            os.mkdir("checkpoint")
+        if not os.path.exists(os.path.join("checkpoint", opt.dataset)):
+            os.mkdir(os.path.join("checkpoint", opt.dataset))
+        net_g_model_out_path = "checkpoint/{}/netG_model_epoch_{}.pth".format(opt.dataset, epoch)
+        net_d_model_out_path = "checkpoint/{}/netD_model_epoch_{}.pth".format(opt.dataset, epoch)
+        torch.save(net_g, net_g_model_out_path)
+        torch.save(net_d, net_d_model_out_path)
+        print("Checkpoint saved to {}".format("checkpoint" + opt.dataset))
