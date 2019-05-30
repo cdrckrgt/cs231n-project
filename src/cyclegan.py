8997b5b110563a05769aa9324d04bf59940b5610
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-05-30 23:25:31 +0000

    bigger generator

--- src/cyclegan.py
+++ src/cyclegan.py
@@ -18,7 +18,7 @@ from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import DataLoader, Dataset
 
 # format is date.run_number this day
-run = '053019.02'
+run = '053019.06'
 if not os.path.exists('../saved_imgs/{}'.format(run)):
     os.mkdir('../saved_imgs/{}'.format(run))
 if not os.path.exists('../weights/{}'.format(run)):
@@ -26,9 +26,9 @@ if not os.path.exists('../weights/{}'.format(run)):
 
 writer = SummaryWriter('../logs/{}'.format(run))
 
-batch_size = 16
-nb_training_iterations = 2000
-lr = 1e-3
+batch_size = 64
+nb_training_iterations = 4000
+lr = 1e-4
 betas = (0.5, 0.999)
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
@@ -37,7 +37,7 @@ dtype = torch.cuda.FloatTensor if device == 'cuda' else torch.FloatTensor
 transform = transforms.Compose([
     transforms.Resize((64, 64)), # downsample the image, too large for efficient training
     transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    # transforms.Normalize((0, 0, 0), (0.3, 0.3, 0.3))
 ])
 
 photo_data = datasets.ImageFolder('../data/sketchydata/photo/', transform)
@@ -63,27 +63,49 @@ class ResnetBlock(nn.Module):
 class Generator(nn.Module):
     def __init__(self):
         super().__init__()
-        
+       
+        # encoder 
         self.conv1 = nn.Conv2d(3, 32, 4, 2, 1)
         self.bn1 = nn.BatchNorm2d(32)
-        self.act1 = nn.ReLU()
+        self.act1 = nn.LeakyReLU(0.2)
         self.conv2 = nn.Conv2d(32, 64, 4, 2, 1)
         self.bn2 = nn.BatchNorm2d(64)
-        self.act2 = nn.ReLU()
-        self.resnet1 = ResnetBlock(64)
-        self.act3 = nn.ReLU()
-        self.convT1 = nn.ConvTranspose2d(64, 32, 4, 2, 1)
-        self.bn3 = nn.BatchNorm2d(32)
-        self.act4 = nn.ReLU()
-        self.convT2 = nn.ConvTranspose2d(32, 3, 4, 2, 1)
-        self.act5 = nn.Tanh()
+        self.act2 = nn.LeakyReLU(0.2)
+        self.conv3 = nn.Conv2d(64, 128, 4, 2, 1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.act3 = nn.LeakyReLU(0.2)
+
+        # resnet
+        self.resnet1 = ResnetBlock(128)
+        self.act4 = nn.LeakyReLU(0.2)
+        self.resnet2 = ResnetBlock(128)
+        self.act5 = nn.LeakyReLU(0.2)
+        self.resnet3 = ResnetBlock(128)
+        self.act6 = nn.LeakyReLU(0.2)
+
+        # decoder
+        self.convT1 = nn.ConvTranspose2d(128, 64, 4, 2, 1)
+        self.bn4 = nn.BatchNorm2d(64)
+        self.act7 = nn.LeakyReLU(0.2)
+        self.convT2 = nn.ConvTranspose2d(64, 32, 4, 2, 1)
+        self.bn5 = nn.BatchNorm2d(32)
+        self.act8 = nn.LeakyRelu(0.2)
+        self.convT3 = nn.ConvTranspose2d(32, 3, 4, 2, 1)
+        self.act9 = nn.Tanh()
 
     def forward(self, x):
+        # encoder
         out = self.act1(self.bn1(self.conv1(x)))
         out = self.act2(self.bn2(self.conv2(out)))
-        out = self.act3(self.resnet1(out))
-        out = self.act4(self.bn3(self.convT1(out)))
-        out = self.act5(self.convT2(out))
+        out = self.act3(self.bn3(self.conv3(out)))
+        # resnet
+        out = self.act4(self.resnet1(out))
+        out = self.act5(self.resnet2(out))
+        out = self.act6(self.resnet3(out))
+        # decoder
+        out = self.act7(self.bn4(self.convT1(out)))
+        out = self.act8(self.bn5(self.convT2(out)))
+        out = self.act9(self.convT3(out))
         return out
 
 
@@ -94,13 +116,13 @@ class Discriminator(nn.Module):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 32, 4, 2, 1)
         self.bn1 = nn.BatchNorm2d(32)
-        self.act1 = nn.ReLU()
+        self.act1 = nn.LeakyReLU(0.2)
         self.conv2 = nn.Conv2d(32, 64, 4, 2, 1)
         self.bn2 = nn.BatchNorm2d(64)
-        self.act2 = nn.ReLU()
+        self.act2 = nn.LeakyReLU(0.2)
         self.conv3 = nn.Conv2d(64, 128, 4, 2, 1)
         self.bn3 = nn.BatchNorm2d(128)
-        self.act3 = nn.ReLU()
+        self.act3 = nn.LeakyReLU(0.2)
 
         self.conv4 = nn.Conv2d(128, 1, 4, 2, 0)
         self.act4 = nn.Sigmoid()
@@ -123,6 +145,7 @@ D_Y = Discriminator().to(device)
 # create optimizers
 G_opt = optim.Adam(list(G_XtoY.parameters()) + list(G_YtoX.parameters()), lr=lr, betas=betas)
 D_opt = optim.Adam(list(D_X.parameters()) + list(D_Y.parameters()), lr=lr, betas=betas)
+# D_opt = optim.SGD(list(D_X.parameters()) + list(D_Y.parameters()), lr=lr, momentum=0.9)
 
 # training loop
 
@@ -175,8 +198,8 @@ sketch_iter = iter(sketch_loader)
 # monitoring batch
 monitor_X, _ = photo_iter.next()
 monitor_Y, _ = sketch_iter.next()
-monitor_X = Variable(monitor_X)
-monitor_Y = Variable(monitor_Y)
+monitor_X = Variable(monitor_X).to(device)
+monitor_Y = Variable(monitor_Y).to(device)
 
 
 for i in range(nb_training_iterations):
@@ -194,9 +217,9 @@ for i in range(nb_training_iterations):
         real_img_X, _ = photo_iter.next()
         real_img_Y, _ = sketch_iter.next()
         
-    real_img_X = Variable(real_img_X)
+    real_img_X = Variable(real_img_X).to(device)
     
-    real_img_Y = Variable(real_img_Y)
+    real_img_Y = Variable(real_img_Y).to(device)
 
     # generate fake images minibatch
     # compute discriminator losses for both domains for fake images
@@ -207,6 +230,9 @@ for i in range(nb_training_iterations):
     D_Y_real_loss = torch.sum(torch.pow(D_Y(real_img_Y) - 1, 2)) / batch_size
 
     D_real_loss = D_X_real_loss + D_Y_real_loss
+
+    # cyclegan implementation does this to slow learning rate of D
+    D_real_loss /= 2
 
     writer.add_scalar('D_real_loss', D_real_loss.item(), global_step=i)
 
@@ -223,6 +249,7 @@ for i in range(nb_training_iterations):
     D_Y_fake_loss = torch.sum(torch.pow(D_Y(fake_img_Y), 2)) / batch_size
 
     D_fake_loss = D_X_fake_loss + D_Y_fake_loss
+    D_fake_loss /= 2
 
     writer.add_scalar('D_fake_loss', D_fake_loss.item(), global_step=i)
 
@@ -282,12 +309,12 @@ for i in range(nb_training_iterations):
         log_train_img(G_XtoY, G_YtoX, monitor_X, monitor_Y, i)
 
     # model checkpointing
-    if i != 0 and i % 250 == 0:
+    if i != 0 and i % 500 == 0:
         torch.save(G_XtoY.state_dict(), '../weights/{0}/G_XtoY.iter_{1}.pt'.format(run, i))
         torch.save(G_YtoX.state_dict(), '../weights/{0}/G_YtoX.iter_{1}.pt'.format(run, i))
         torch.save(D_X.state_dict(), '../weights/{0}/D_X.iter_{1}.pt'.format(run, i))
         torch.save(D_Y.state_dict(), '../weights/{0}/D_Y.iter_{1}.pt'.format(run, i))
-        log_train_img(G_XtoY, G_YtoX, monitor_X, monitor_Y, i, save=True)
+        # log_train_img(G_XtoY, G_YtoX, monitor_X, monitor_Y, i, save=True)
 
 torch.save(G_XtoY.state_dict(), '../weights/{0}/G_XtoY.pt'.format(run))
 torch.save(G_YtoX.state_dict(), '../weights/{0}/G_YtoX.pt'.format(run))
